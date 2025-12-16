87027d7ad4267102fb54f4080e1d032ab0e78335
Aimane01 <aimane.alassane@epitech.eu>
2025-12-16 01:11:52 +0100

    fix(IArtificialNeuron): adding a getType method to the interface

--- include/IArtificialNeuron.hpp
+++ include/IArtificialNeuron.hpp
@@ -11,4 +11,5 @@ class IArtificialNeuron {
         */
         virtual std::vector<float>& get_weights(void) = 0;
         virtual float& get_bias() = 0;
+        virtual NeuronType getType() const = 0;
 };
