1cc544e16b3c4efdf0ac222acc63b8337ce14daa
Aimane01 <aimane.alassane@epitech.eu>
2025-12-16 02:26:46 +0100

    fix(Perceptron): adding a n_weights variable to the parameters of the constructor

--- include/Perceptron.hpp
+++ include/Perceptron.hpp
@@ -9,69 +9,21 @@ class Perceptron : public IArtificialNeuron {
             int n_weights,
             std::optional<std::vector<float>> weights = std::nullopt,
             std::optional<float> bias = std::nullopt
-        )
-        : m_nWeights(n_weights)
-        {
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_real_distribution<float> dist(-1.f, 1.f);
-    
-            if (weights) {
-                m_weights = std::move(*weights);
-                if (m_weights.size() != m_nWeights)
-                    throw std::invalid_argument("Weights size does not match n_weights");
-            } else {
-                m_weights.resize(m_nWeights);
-                for (auto& w : m_weights) w = dist(gen);
-            }
-    
-            m_bias = bias.value_or(dist(gen));
-            m_z = 0.f;
-            m_output = 0.f;
-        }
+        );
         
-        float compute_z(const std::vector<float>& inputs) override {
-            if (inputs.size() != m_weights.size()) {
-                throw std::invalid_argument(
-                    "Input size (" + std::to_string(inputs.size()) +
-                    ") does not match number of weights (" + std::to_string(m_weights.size()) + ")"
-                );
-            }
+        float compute_z(const std::vector<float>& inputs) override;
     
-            float z = m_bias;
-            for (size_t i = 0; i < m_weights.size(); ++i) {
-                z += m_weights[i] * inputs[i];
-            }
+        float compute_activation() override;
     
-            m_z = z;
-            return z;
-        }
+        float get_output() override;
     
-        float compute_activation() override {
-            // Step function: output = 1 si z >= 0, sinon 0
-            m_output = (m_z >= 0.f) ? 1.f : 0.f;
-            return m_output;
-        }
+        std::vector<float>& get_weights() override;
     
-        float get_output() override {
-            return m_output;
-        }
+        float& get_bias() override;
     
-        std::vector<float>& get_weights() override {
-            return m_weights;
-        }
+        float& get_z() override;
     
-        float& get_bias() override {
-            return m_bias;
-        }
-    
-        float& get_z() override {
-            return m_z;
-        }
-    
-        NeuronType getType() const override {
-            return NeuronType::PERCEPTRON;
-        }
+        NeuronType getType() const override;
     
     private:
         size_t m_nWeights;
@@ -79,5 +31,6 @@ class Perceptron : public IArtificialNeuron {
         float m_bias;
         float m_z;
         float m_output;
+        float m_activation;
     };
     
