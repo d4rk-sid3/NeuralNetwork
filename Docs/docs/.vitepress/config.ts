5e92c7430b14c4ceff126a009034703ea5a122e0
Jordan AMOUZOUN <jordan.amouzoun@epitech.eu>
2025-12-17 15:20:23 +0100

    feat: Initialise the project documentation

--- Docs/docs/.vitepress/config.ts
+++ Docs/docs/.vitepress/config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vitepress';
 // see https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
-  title: 'Glados - 2k25',
+  title: 'MY_TOUCH - 2k25',
   base: process.env.NODE_ENV === 'production' ? '/G-CPP-500-COT-5-1-rtype-22/Docs/' : '/',
-  description: 'Glados project documentation',
+  description: 'MY_TOUCH project documentation',
   
   head: [
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap', rel: 'stylesheet' }],
     ['script', { src: 'https://code.iconify.design/iconify-icon/1.0.8/iconify-icon.min.js' }],
-    ['meta', { name: 'theme-color', content: '#ff0000ff' }],
+    ['meta', { name: 'theme-color', content: '#1100ffff' }],
   ],
 
   themeConfig: {
@@ -27,7 +27,7 @@ export default defineConfig({
           { text: 'Developer Guide', link: '/Developer/CONTRIBUTING' },
           { text: 'User Guide', link: '/Developer/USER_GUIDE' },
           { text: 'Build & Deploy', link: '/Build/BUILD_GUIDE' },
-          { text: 'Project Details', link: '/Glados' },
+          { text: 'Project Details', link: '/MY_TOUCH' },
         ]
       },
     ],
@@ -36,7 +36,7 @@ export default defineConfig({
       {
         text: 'Overview',
         items: [
-          { text: 'Glados Project', link: '/' },
+          { text: 'MY_TOUCH Project', link: '/' },
           { text: 'Our Team', link: '/TEAM_ORGANIZATION' },
         ],
       },
@@ -48,10 +48,22 @@ export default defineConfig({
         ],
       },
       {
-        text: 'Technical Study',
+        text: 'Technical documentation',
         collapsed: true,
         items: [
-          { text: 'Technical study', link: '/Technical/TECHNICAL_DOCUMENTATION' },
+          { text: 'Technical documentation', link: '/Technical/TECHNICAL_DOCUMENTATION' },
+          { text: 'FEN to Neural Network Input Encoding Specification', link: '/Technical/how_to_convert' },
+          { text: 'Hyperparameters saving', link: '/Technical/hyperparameters_saving' },
+        ],
+      },
+      {
+        text: 'Benchmarking',
+        collapsed: false,
+        items: [
+          { text: 'Benchmark data convention', link: '/Benchmark/benchmark_JSON_Convention' },
+          { text: 'Benchmark structure', link: '/Benchmark/benchmark_structure' },
+          { text: 'Benchmark technical documentation', link: '/Benchmark/benchmark_technical_documentation' },
+          { text: 'Run and visualize benchmark', link: '/Benchmark/startBenchMark' },
         ],
       },
       {
