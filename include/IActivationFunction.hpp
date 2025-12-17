2ef357e6e493eb573bf7d1cfae8bdeaf26a2bbdb
Aimane01 <aimane.alassane@epitech.eu>
2025-12-17 02:50:44 +0100

    feat(IActivationFunction): adding a derivative abstract method to the interface

--- include/IActivationFunction.hpp
+++ include/IActivationFunction.hpp
@@ -4,4 +4,5 @@ class IActivationFunction {
     public:
         virtual float apply(const float z) const = 0;
         virtual ActivationFunctionType getType() const = 0;
+        virtual float derivative(float z) const = 0;
 };
