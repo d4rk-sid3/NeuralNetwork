6922a1bfb1b9cbe399106a37d6315e8409ead8b0
Aimane01 <aimane.alassane@epitech.eu>
2025-12-18 20:52:42 +0100

    fix(DataLoader.cpp): variable selection to avoid noise with the model

--- include/DataLoader.cpp
+++ include/DataLoader.cpp
@@ -3,6 +3,7 @@
 std::vector<TrainingSample> DataLoader::loadData(const std::string& filename)
 {
     std::ifstream file(filename);
+
     if (!file.is_open()) {
         throw std::runtime_error("Unable to open data file: " + filename);
     }
@@ -10,33 +11,46 @@ std::vector<TrainingSample> DataLoader::loadData(const std::string& filename)
     std::vector<TrainingSample> dataset;
     std::string line;
 
+    constexpr size_t TOTAL_VALUES = 78;
+    constexpr size_t OUTPUT_SIZE  = 5;
+    constexpr size_t INPUT_SIZE   = 65;
+
     while (std::getline(file, line)) {
-        if (line.empty()) continue;
+        if (line.empty())
+            continue;
 
         std::stringstream ss(line);
         std::vector<float> values;
         float v;
 
-        // Read all the values on the line
         while (ss >> v) {
             values.push_back(v);
         }
 
-        // Check the validity of the line
-        if (values.size() != 78) {
-            throw std::runtime_error("Invalid data line: not enough values. Expected number of values : 78");
+        if (values.size() != TOTAL_VALUES) {
+            throw std::runtime_error(
+                "Invalid data line: expected 78 values, got " +
+                std::to_string(values.size())
+            );
         }
 
-        // Input / output separation
-        const size_t output_size = 5;
-        const size_t input_size = values.size() - output_size;
+        std::vector<float> input;
+        input.reserve(INPUT_SIZE);
 
-        std::vector<float> input(values.begin(), values.begin() + input_size);
-        std::vector<float> output(values.begin() + input_size, values.end());
+        // Board squares (64)
+        input.insert(input.end(), values.begin(), values.begin() + 64);
+
+        // Side to move (1)
+        input.push_back(values[64]);
+
+        // Build OUTPUT (5 values)
+        std::vector<float> output(
+            values.end() - OUTPUT_SIZE,
+            values.end()
+        );
 
         dataset.emplace_back(std::move(input), std::move(output));
     }
-
     return dataset;
 }
 
@@ -83,7 +97,7 @@ std::vector<std::vector<float>> DataLoader::load_weights(const std::string& path
 std::vector<float> DataLoader::load_biases(const std::string& path)
 {
     std::ifstream file(path, std::ios::binary);
-    
+
     if (!file.is_open()) {
         throw std::runtime_error("Cannot open biases file: " + path);
     }
