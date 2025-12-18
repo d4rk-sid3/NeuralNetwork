2515caefffc609a0ec31f071dfa3a9a0a16d2589
Aimane01 <aimane.alassane@epitech.eu>
2025-12-18 20:55:00 +0100

    fix(ExperimentRunner.hpp): creating a loadnetwork method

--- include/ExperimentRunner.hpp
+++ include/ExperimentRunner.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "librairies.hpp"
+#include "NeuralNetwork.hpp"
 
 class ExperimentRunner {
     public:
+        NeuralNetwork loadNetwork(const std::string& network_name);
 };
